7c0659b0ffbcce1fac4cb947a4831787b1c2e35a
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-04-02 19:08:38 -0600

    haciendo rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [
